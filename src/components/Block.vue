60f235ff3d9969c6cabadd9b579c82254eddb934
Sadiq O <sadiqomar890@gmail.com>
2021-08-22 19:52:16 +0300

    made some changes

---
+++ src/components/Block.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="block">
+    click me
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Block",
+};
+</script>
+
+<style scoped>
+.block {
+  background-color: rgb(0, 174, 255);
+  width: 40%;
+  margin: auto;
+  padding: 7rem;
+  border-radius: 0.7rem;
+  position: relative;
+  top: 2rem;
+}
+</style>
